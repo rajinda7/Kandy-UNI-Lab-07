fa0d20ce845664f4a3391b57a3fa693fcb0ab996
rajinda7 <IT26101294@my.sliit.lk>
2026-09-13 16:10:31 +0530

    Add files via upload

---
+++ Q2/IT26101294Lab7Q2B.java
@@ -0,0 +1,11 @@
+public class IT26101294Lab7Q2B {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " - ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
